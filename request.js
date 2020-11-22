6952dcc14fb01e03cae7d6e2aa39d99fd9c53de4
Koby Picker <jkp46@case.edu>
2020-11-21 23:27:26 -0500

    move csv functionality to request

--- request.js
+++ request.js
@@ -4,19 +4,21 @@ const rp = require('request-promise')
 const fs = require('fs');
 let conf = fs.readFileSync('token.json').toString();
 conf = JSON.parse(conf);
-jwt = conf.jwt;
-meeting = process.argv[2]
-path = `/meetings/${meeting}/registrants`
+let jwt = conf.jwt;
+let meeting = process.argv[2]
+let occurrence = process.argv.length > 3 && process.argv[3]
+let participants = occurrence === "-p";
+let path = participants ? `/report/meetings/${meeting}/participants` : `/meetings/${meeting}/registrants`;
 
 if (!meeting) {
-  console.error("Argument required: meeting id")
+  console.error("Usage: request.js <meetingId> [<occurenceId>|-p]")
   process.exit(1);
 }
-
 var options = {
     uri: 'https://api.zoom.us/v2' + path,
     qs: {
-      page_size: 100
+      page_size: 300,
+      occurence_id: !participants ? occurrence : undefined
     },
     auth: {
       //Provide your token here
@@ -29,10 +31,28 @@ var options = {
     json: true // Automatically parses the JSON string in the response
 };
 
+let tocsv = (resp) => {
+  let people;
+  if (resp.registrants) {
+    people = resp.registrants
+    console.log("first, last, email");
+    for (const p of people) {
+      console.log(`${p.first_name}, ${p.last_name || ''}, ${p.email}`)
+    }
+  }
+  if (resp.participants) {
+    people = resp.participants;
+    console.log("name, email");
+    for (const p of people) {
+      console.log(`${p.name}, ${p.user_email}`)
+    }
+  }
+}
+
 rp(options)
     .then(function (response) {
       //logic for your response
-        console.log(JSON.stringify(response.registrants));
+      tocsv(response)
     })
     .catch(function (err) {
         // API call failed...
--- tocsv.js
+++ tocsv.js
@@ -2,9 +2,20 @@
 
 const fs = require('fs');
 let stdin = fs.readFileSync('/dev/stdin').toString();
-registrants = JSON.parse(stdin);
-
-console.log("first, last, email");
-for (const reg of registrants) {
-  console.log(`${reg.first_name}, ${reg.last_name || ''}, ${reg.email}`)
+const blob = JSON.parse(stdin);
+let people;
+if (blob.registrants) {
+  people = blob.registrants
+  console.log("first, last, email");
+  for (const p of people) {
+    console.log(`${p.first_name}, ${p.last_name || ''}, ${p.email}`)
+  }
 }
+if (blob.participants) {
+  people = blob.participants;
+  console.log("name, email, duration (s)");
+  for (const p of people) {
+    console.log(`${p.name}, ${p.user_email}`)
+  }
+}
+
